0db859a00502f0119c850b13ab60392574d2893f
Yishai Zehavi <zehaviyishai@gmail.com>
2020-09-14 10:39:40 +0300

    configured react with tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from './components/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
+      <h1>Game of Life</h1>
+      <Button onClick={() => console.log('I was clicked')}>
+        Start the game
+      </Button>
     </div>
   );
 }
